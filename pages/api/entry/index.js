b64e024094482e0adee2242371a5b4bb5ba38abe
Gokul Sundar <gokul@walnuts.co.in>
2023-03-06 22:11:13 +0530

    Error handling, retries and mobile no. validation

--- pages/api/entry/index.js
+++ pages/api/entry/index.js
@@ -1,4 +1,21 @@
-import db from '../../../utils/db';
+import db from "../../../utils/db";
+
+function validateMobileNumber(number) {
+  const re = /^[0-9]{10}$/;
+  return re.test(number);
+}
+
+async function getEntryIfNumberRegistered(number) {
+  const collectionRef = db.collection("entries");
+  const querySnapshot = await collectionRef
+    .where("mobileNo", "==", number)
+    .get();
+  if (querySnapshot.empty) {
+    return false;
+  } else {
+    return querySnapshot.docs[0].data();
+  }
+}
 
 export default async (req, res) => {
   try {
@@ -8,16 +25,48 @@ export default async (req, res) => {
 
     // if (entriesData.some(entry => entry.mobile === mobile)) {
     //   res.status(400).end();
-    // } 
-    // else {
-      const { id } = await db.collection('entries').add({
-        ...req.body,
-        created: new Date().toISOString(),
-      });
-      res.status(200).json({ id });
     // }
+    // else {
+    let member = await getEntryIfNumberRegistered(req.body.mobileNo);
+    if (!validateMobileNumber(req.body.mobileNo)) {
+      res.status(400).json({ error: "Mobile number is not valid" });
+      return;
+    }
+    if (member) {
+      res.status(400).json({ error: "Mobile number is already registered" });
+      return;
+    }
+
+    // Try couple times to enter the info with the generated id in case of 
+    // competing attempts to enter data into entries.
+    const maxAttempts = 5;
+    let attempts = maxAttempts;
+    while (--attempts >= 0) {
+      let newMemberId = db.collection("computed").doc("stats").get().newID;
+      
+      try {
+        const { id } = await db.collection("entries").add({
+          ...req.body,
+          created: new Date().toISOString(),
+          memberId: newMemberId
+        });
+        // Successfully added new members to entries.
+        res.status(200).json({ id });
+        return;
+      }
+      // memberId already exists. Try again if possible.
+      catch (e) {
+        console.error(e, "Failed to save member id. Trying again.");
+        // TODO: Verify that we throw an exception if member id already exists.
+        continue;
+      }
+    }
+
+    res.status(400).json({ error: "Catastrophic error: Failed to save member id even after " + maxAttempts + " attempts." });
+    return;
+    
   } catch (e) {
-    console.log('db not connected')
-    res.status(400).end();
+    console.log("db not connected",e);
+    res.status(400).json({ error: "something went wrong :" + e.message });
   }
-}
+};
